44e2d114cb0ea8f03bdf9c0f3438cdd9feac848a
Markuss <markuss299@gmail.com>
2021-12-02 14:53:04 +0000

    Header now responsive for other window sizes.

--- ernoid/src/components/Header.js
+++ ernoid/src/components/Header.js
@@ -1,14 +1,31 @@
-import React, {Component, useEffect, useState} from 'react';
+import React, {Component, useEffect, useLayoutEffect, useState} from 'react';
 import { Grid } from '@material-ui/core';
 import Button from "react-bootstrap/cjs/Button"
 import Dropdown from "react-bootstrap/cjs/Dropdown";
 import store from "../redux/store";
 import { Link, useLocation } from "react-router-dom";
 
+function useWindowSize() {
+    const [size, setSize] = useState([0, 0]);
+
+    useLayoutEffect(() => {
+
+        function updateSize() {
+            setSize([window.innerWidth, window.innerHeight]);
+        }
+
+        window.addEventListener('resize', updateSize);
+        updateSize();
+        return () => window.removeEventListener('resize', updateSize);
+
+    }, []);
+
+    return size;
+}
+
 /**
  * The header file is responsible for the header bar and main navigation functionality.
  */
-
 export default function Header() {
 
     let location = useLocation();
@@ -16,9 +33,9 @@ export default function Header() {
     const [collections, setCollections] = useState([]);
     const [count, setCount] = useState(store.getState().shop.itemsInCart);
     const [pathname, setPathname] = useState(location.pathname);
+    const [width, height] = useWindowSize()
 
-    useEffect(
-        () => {
+    useEffect(() => {
             setPathname(location.pathname); // Used for setting color of header location to white based on path
         }
     )
@@ -53,49 +70,61 @@ export default function Header() {
 
     }
 
+    function loadHeaderImage() {
+        if (width > 720) {
+            return <img className={"header-logo"} src={"/static/images/branding/ERNOID_Typography_White.png"}/>
+        } else {
+            return <img className={"header-logo-small"} src={"/static/images/branding/ERNOID_Logo_White.png"}/>
+        }
+    }
+
     return (
         <div className={"header-style"}>
             <Grid container direction="row" justify="center" alignItems="center">
 
                 <Link to={"/"}>
-                    <img className={"header-logo"} src={"/static/images/branding/ERNOID_Typography_White.png"}/>
+                    {loadHeaderImage()}
                 </Link>
 
-                <div className={"header-navigation"}>
+                <div className={width > 720 ? "header-navigation" : "header-navigation-small"}>
                     <Grid container direction="row" justify="center" alignItems="center">
                         <Button className={"header-dropdown-home"} as={Link} to={"/"}
                                 style={pathname==="/" ? {color: 'white'} : {color: '#828282'}}>HOME</Button>
-                        <Dropdown>
-                          <Dropdown.Toggle className={"header-dropdown-toggle"}
-                                           style={pathname==="/all-collections" || pathname.includes("/collection/") ? {color: 'white'} : {color: '#828282'}}>COLLECTIONS</Dropdown.Toggle>
-                            {/* TODO: Dynamically add collections to the menu page */}
-                            <Dropdown.Menu className={"header-dropdown-menu"}>
-                                    <Dropdown.Item className={"header-dropdown-item"} as={Link} to="../all-collections">ALL COLLECTIONS</Dropdown.Item>
-                                <Dropdown.Item className={"header-dropdown-item"} as={Link} to="../collection/classic">CLASSIC</Dropdown.Item>
-                                <Dropdown.Item className={"header-dropdown-item"} as={Link} to="../collection/tilted-colors">TILTED COLORS</Dropdown.Item>
-                                <Dropdown.Item className={"header-dropdown-item"} as={Link} to="../collection/censored">CENSORED</Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown>
+                        <div className={"header-dropdown-container"}>
+                            <Dropdown>
+                              <Dropdown.Toggle className={"header-dropdown-toggle"}
+                                               style={pathname==="/all-collections" || pathname.includes("/collection/") ? {color: 'white'} : {color: '#828282'}}>COLLECTIONS</Dropdown.Toggle>
+                                {/* TODO: Dynamically add collections to the menu page */}
+                                <Dropdown.Menu className={"header-dropdown-menu"}>
+                                        <Dropdown.Item className={"header-dropdown-item"} as={Link} to="../all-collections">ALL COLLECTIONS</Dropdown.Item>
+                                    <Dropdown.Item className={"header-dropdown-item"} as={Link} to="../collection/classic">CLASSIC</Dropdown.Item>
+                                    <Dropdown.Item className={"header-dropdown-item"} as={Link} to="../collection/tilted-colors">TILTED COLORS</Dropdown.Item>
+                                    <Dropdown.Item className={"header-dropdown-item"} as={Link} to="../collection/censored">CENSORED</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </div>
 
-                        <Dropdown>
-                          <Dropdown.Toggle className={"header-dropdown-toggle"}
-                                           style={pathname.includes("/clothes/") ? {color: 'white'} : {color: '#828282'}}>CLOTHES</Dropdown.Toggle>
+                        <div className={"header-dropdown-container"}>
+                            <Dropdown>
+                              <Dropdown.Toggle className={"header-dropdown-toggle"}
+                                               style={pathname.includes("/clothes/") ? {color: 'white'} : {color: '#828282'}}>CLOTHES</Dropdown.Toggle>
 
-                            {/* TODO: dynamic arī vajadzētu :) */}
-                          <Dropdown.Menu className={"header-dropdown-menu"}>
-                            <Dropdown.Item className={"header-dropdown-item"} as={Link} to="../clothes/t-shirt">T-SHIRT</Dropdown.Item>
-                            <Dropdown.Item className={"header-dropdown-item"} as={Link} to="../clothes/sweatshirt">SWEATSHIRT</Dropdown.Item>
-                            <Dropdown.Item className={"header-dropdown-item"} as={Link} to="../clothes/hoodie">HOODIE</Dropdown.Item>
-                            <Dropdown.Item className={"header-dropdown-item"} as={Link} to="../clothes/dress">DRESS</Dropdown.Item>
-                            <Dropdown.Item className={"header-dropdown-item"} as={Link} to="../clothes/beanie">BEANIE</Dropdown.Item>
-                          </Dropdown.Menu>
-                        </Dropdown>
+                                {/* TODO: dynamic arī vajadzētu :) */}
+                              <Dropdown.Menu className={"header-dropdown-menu"}>
+                                <Dropdown.Item className={"header-dropdown-item"} as={Link} to="../clothes/t-shirt">T-SHIRT</Dropdown.Item>
+                                <Dropdown.Item className={"header-dropdown-item"} as={Link} to="../clothes/sweatshirt">SWEATSHIRT</Dropdown.Item>
+                                <Dropdown.Item className={"header-dropdown-item"} as={Link} to="../clothes/hoodie">HOODIE</Dropdown.Item>
+                                <Dropdown.Item className={"header-dropdown-item"} as={Link} to="../clothes/dress">DRESS</Dropdown.Item>
+                                <Dropdown.Item className={"header-dropdown-item"} as={Link} to="../clothes/beanie">BEANIE</Dropdown.Item>
+                              </Dropdown.Menu>
+                            </Dropdown>
+                        </div>
 
                         <p className={"header-separator"}>|</p>
                         <Link className={"header-cart"} to={"/shopping-cart"}>
                                 <img className={"header-cart-image"} src={"/static/images/assets/cart_white.png"} />
                         </Link>
-                        <p className={"header-separator"}>{count}</p>
+                        <p className={"header-count"}>{count}</p>
 
                     </Grid>
                 </div>
