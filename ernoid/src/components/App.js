7a66e2e396b7f828648d76e1cb809e53ceaae83f
Markuss <markuss299@gmail.com>
2021-12-02 15:48:04 +0000

    Fixed overflowing x for whole page

--- ernoid/src/components/App.js
+++ ernoid/src/components/App.js
@@ -31,7 +31,7 @@ export default class App extends Component {
 
     render() {
         return (
-            <Grid style={{overflowY: 'scroll'}}>
+            <Grid style={{overflowY: 'scroll', overflowX: 'hidden'}}>
                 <Router>
                     <Header/>
                     <Switch>
